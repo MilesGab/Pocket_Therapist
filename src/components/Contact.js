f60a502b0c2200dcae02d95c3dfbf7ef3b0b0aa0
MicoRxz <139572357+MicoRxz@users.noreply.github.com>
2023-08-22 17:46:45 +0800

    contact page
    implemented UI

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,28 +1,37 @@
-import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import React, { useState, useCallback, useEffect } from 'react'
+import { GiftedChat } from 'react-native-gifted-chat'
 
-const Contact = () => {
+export default function Contact() {
+  const [messages, setMessages] = useState([])
+
+  useEffect(() => {
+    setMessages([
+      {
+        _id: 1,
+        text: 'Hello developer',
+        createdAt: new Date(),
+        user: {
+          _id: 2,
+          name: 'React Native',
+          avatar: 'https://placeimg.com/140/140/any',
+        },
+      },
+    ])
+  }, [])
+
+  const onSend = useCallback((messages = []) => {
+    setMessages(previousMessages =>
+      GiftedChat.append(previousMessages, messages),
+    )
+  }, [])
+
   return (
-    <View style={styles.container}>
-      <View id="header">
-        <Text style={{fontSize: 32, color: 'black'}}>Chat</Text>
-      </View>
-      <View id="services">
-        <Text>Services</Text>
-            <View>
-            </View>
-      </View>
-    </View>
-  );
-};
-
-const styles = StyleSheet.create({
-  container: {
-    backgroundColor: '#1C6BA4',
-    flex: 1,
-    justifyContent: 'space-between',
-    padding: 20,
-  },
-});
-
-export default Contact;
+    <GiftedChat
+      messages={messages}
+      onSend={messages => onSend(messages)}
+      user={{
+        _id: 1,
+      }}
+    />
+  )
+}
