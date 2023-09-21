06475c2facf75d2b80c648414cb26f23c42c2e92
dustinn22 <dustinn2202@gmail.com>
2023-09-21 16:06:44 +0800

    Made changes in the structure of the Questionnaire file itself
    Splitted the Physical and Pain Questions into components

--- src/screens/Assessment/Questionnaire.js
+++ src/screens/Assessment/Questionnaire.js
@@ -8,97 +8,61 @@ import Slider from "@react-native-community/slider";
 import DropDownPicker from "react-native-dropdown-picker";
 
 function Questionnaire() {
-  // State for tracking the current question, answers, and selected image
+
+  const questions = [
+    'Question 1: Is there discoloration on the wrist?',
+    'Question 2: Is there swelling on the wrist?',
+    'Question 3: Is the wrist warm to touch? (Warmer than usual)',
+    'Question 4: Are you experiencing any kind of pain when you touch or apply pressure to your wrist?',
+    'Take a picture of your wrist in different angles',
+  ];
+
+  const painAssessmentQuestions = [
+    'Question 1: On a scale of 0-10, how would you rate your pain?',
+    'Question 2: What type of pain are you experiencing?',
+  ];
+
+  const painChoices = [
+    {label: 'Sharp', value: 'Sharp'},
+    {label: 'Dull', value: 'Dull'},
+    {label: 'Numb', value: 'Numb'},
+    // Add more questions as needed
+  ];
+
   const [currentQuestion, setCurrentQuestion] = useState(0);
   const [answers, setAnswers] = useState(Array(questions.length).fill(''));
   const [selectedImage, setSelectedImage] = useState(null);
   const [sliderValue, setSliderValue] = useState(0);
-  const [selectedPain, setSelectedPain] = useState(null); // Selected pain choice
-  const [isOthersSelected, setIsOthersSelected] = useState(false); //2nd question Pain assessment
-
-  // State for tracking whether the user is in the pain assessment section
+  const [selectedPain, setSelectedPain] = useState(null);
+  const [isOthersSelected, setIsOthersSelected] = useState(false);
   const [isPainAssessment, setIsPainAssessment] = useState(false);
-
-  // State for tracking the current pain assessment question
   const [currentPainQuestion, setCurrentPainQuestion] = useState(0);
 
-  // Function to handle moving to the next question in the physical assessment
   const handleNextQuestion = () => {
     if (currentQuestion < questions.length - 1) {
       setCurrentQuestion(currentQuestion + 1);
     }
   };
 
-  // Function to handle moving to the previous question in the physical assessment
   const handlePreviousQuestion = () => {
     if (currentQuestion > 0) {
       setCurrentQuestion(currentQuestion - 1);
     }
   };
 
-  // Function to handle moving to the next pain assessment question
   const handleNextPainQuestion = () => {
     if (currentPainQuestion < painAssessmentQuestions.length - 1) {
       setCurrentPainQuestion(currentPainQuestion + 1);
     }
   };
 
-  // Function to handle moving to the previous pain assessment question
   const handlePreviousPainQuestion = () => {
     if (currentPainQuestion > 0) {
       setCurrentPainQuestion(currentPainQuestion - 1);
     }
   };
 
-  // Function to pick an image from the device's library
-  const handleImageLibraryPicker = () => {
-    // Configure the image picker options (you can customize this)
-    const options = {
-      title: 'Select Image',
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-    // Show the image picker dialog
-    launchImageLibrary(options, (response) => {
-      if (response.didCancel) {
-        console.log('User cancelled image picker');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else {
-        // Set the selected image
-        setSelectedImage(response);
-      }
-    });
-  };
-
-  // Function to capture a photo using the device's camera
-  const handleCameraPicker = () => {
-    const options = {
-      title: 'Take a Picture',
-      storageOptions: {
-        skipBackup: true,
-        path: 'images',
-      },
-    };
-
-    launchCamera(options, (response) => {
-      if (response.didCancel) {
-        console.log('User cancelled image capture');
-      } else if (response.error) {
-        console.log('ImagePicker Error: ', response.error);
-      } else {
-        setSelectedImage(response);
-      }
-    });
-  };
-
-  // Calculate progress based on the current question
   const progress = ((currentQuestion + 1) / questions.length) * 100;
-
-  // Check if all previous questions have been answered to enable proceeding to Pain Assessment
-  //const canProceedToPainAssessment = answers.every(answer => answer !== '');
 
   const formatSliderValue = (value) => {
     return value.toString();
@@ -113,67 +77,30 @@ function Questionnaire() {
 
       <View style={styles.top}>
         <ProgressBar progress={progress / 100} color="#f9bc27" />
-        {isPainAssessment ? (
-          <View>
-            <Text style={styles.question}>
-              {painAssessmentQuestions[currentPainQuestion]}
-            </Text>
-            {currentPainQuestion === 0 && (
-              <View>
-                <Slider
-                  style={styles.slider}
-                  minimumValue={0}
-                  maximumValue={10}
-                  step={1}
-                  minimumTrackTintColor="blue"
-                  maximumTrackTintColor="lightgray"
-                  thumbTintColor="blue"
-                  value={sliderValue}
-                  onValueChange={(value) => setSliderValue(value)}
-                />
-                <View style={styles.sliderScale}>
-                  {Array.from({ length: 11 }, (_, i) => (
-                    <Text key={i}>{i}</Text>
-                  ))}
-                </View>
-                <Text style={styles.sliderValue}>{sliderValue.toString()}</Text>
-              </View>
-            )}
 
-            {currentPainQuestion === 1 && (
-              <View>
-                <DropDownPicker
-                  items={painChoices.map((choice) => ({
-                    label: choice,
-                    value: choice,
-                  }))}
-                  defaultValue={selectedPain}
-                  containerStyle={{ height: 40 }}
-                  onChangeItem={(items) => {
-                    setSelectedPain(items.value);
-                    setIsOthersSelected(items.value === 'Others');
-                  }}
-                />
-                {isOthersSelected && (
-                  <TextInput
-                    placeholder="Please specify"
-                    onChangeText={(text) => {
-                      // Handle the user's input here
-                    }}
-                  />
-                )}
-              </View>
-            )}
-          </View>
+        {isPainAssessment ? (
+          <PainAssessmentQuestions
+            currentPainQuestion={currentPainQuestion}
+            painAssessmentQuestions={painAssessmentQuestions}
+            sliderValue={sliderValue}
+            selectedPain={selectedPain}
+            isOthersSelected={isOthersSelected}
+            setSelectedPain={setSelectedPain}
+            setIsOthersSelected={setIsOthersSelected}
+            setSliderValue={setSliderValue}
+          />
         ) : (
-          <Text style={styles.question}>{questions[currentQuestion]}</Text>
+          <PhysicalInspectionQuestions
+            currentQuestion={currentQuestion}
+            questions={questions}
+            isPainAssessment={isPainAssessment}
+          />
         )}
       </View>
-      {/* Display physical assessment questions or image picker buttons */}
+
       {currentQuestion !== questions.length - 1 ? (
-        // Display physical assessment questions
         <View>
-           {isPainAssessment ? null : ( // Conditionally render the "YES" and "NO" buttons
+          {isPainAssessment ? null : (
             <React.Fragment>
               <TouchableOpacity style={styles.ansButton}>
                 <Text style={styles.ansButtonText}>YES</Text>
@@ -184,36 +111,12 @@ function Questionnaire() {
             </React.Fragment>
           )}
         </View>
-      ) : (
-        // Render buttons for image library and camera access
-        // Camera buttons will only show when it is in Physical Inspection part of the assessment
-        !isPainAssessment && (
-        <View>
-          <TouchableOpacity
-            onPress={handleImageLibraryPicker}
-            style={styles.imagePicker}
-          >
-            <Icon name="image-outline" size={30} color="white" />
-            <Text style={styles.imagePickerText}>Select from Gallery</Text>
-          </TouchableOpacity>
+      ) : null /* No image selection buttons */}
 
-          <TouchableOpacity
-            onPress={handleCameraPicker}
-            style={styles.cameraPicker}
-          >
-            <Icon name="camera-outline" size={30} color="white" />
-            <Text style={styles.cameraPickerText}>Take a Picture</Text>
-          </TouchableOpacity>
-        </View>
-        )
-      )}
-
-      {/* Display the selected image */}
       {selectedImage && (
         <Image source={{ uri: selectedImage.uri }} style={styles.selectedImage} />
       )}
 
-      {/* Navigation buttons (BACK, NEXT, Proceed to Pain Assessment) */}
       <View style={styles.bottom}>
         <TouchableOpacity
           onPress={isPainAssessment ? handlePreviousPainQuestion : handlePreviousQuestion}
@@ -226,19 +129,6 @@ function Questionnaire() {
         >
           <Text style={styles.buttonText}>BACK</Text>
         </TouchableOpacity>
-
-        {/* {currentQuestion === questions.length - 1 && (
-          <TouchableOpacity
-            onPress={() => setIsPainAssessment(true)}
-            disabled={!canProceedToPainAssessment}
-            style={[
-              styles.button,
-              !canProceedToPainAssessment && { backgroundColor: 'gray' },
-            ]}
-          >
-            <Text style={styles.buttonText}>Proceed to Pain Assessment</Text>
-          </TouchableOpacity>
-        )} */}
 
         <TouchableOpacity
           onPress={isPainAssessment ? handleNextPainQuestion : handleNextQuestion}
@@ -253,7 +143,6 @@ function Questionnaire() {
         </TouchableOpacity>
       </View>
 
-      {/* Toggle button for switching between Physical Inspection and Pain Assessment */}
       <TouchableOpacity
         onPress={() => setIsPainAssessment(!isPainAssessment)}
         style={styles.toggleButton}
@@ -266,33 +155,115 @@ function Questionnaire() {
   );
 }
 
-const questions = [
-  'Question 1: Is there discoloration on the wrist?',
-  'Question 2: Is there swelling on the wrist?',
-  'Question 3: Is the wrist warm to touch? (Warmer than usual)',
-  'Question 4: Are you experiencing any kind of pain when you touch or apply pressure to your wrist?',
-  'Take a picture of your wrist in different angles',
-];
 
-const painAssessmentQuestions = [
-  'Question 1: Rate your pain from 1 - 10 (1 - lowest 10 - highest)',
-  'Question 2: Describe the pain based on the choices provided',
-  'Question 3: How long have you been experiencing pain on your wrist?',
-];
+function PainAssessmentQuestions({
+  currentPainQuestion,
+  painAssessmentQuestions,
+  sliderValue,
+  selectedPain,
+  isOthersSelected,
+  setSelectedPain,
+  setIsOthersSelected,
+  setSliderValue,
+}) {
+  const renderPainAssessmentQuestion = () => {
+    if (currentPainQuestion === 0) {
+      return (
+        <View>
+          <Slider
+            style={styles.slider}
+            minimumValue={0}
+            maximumValue={10}
+            step={1}
+            minimumTrackTintColor="blue"
+            maximumTrackTintColor="lightgray"
+            thumbTintColor="blue"
+            value={sliderValue}
+            onValueChange={(value) => setSliderValue(value)}
+          />
+          <View style={styles.sliderScale}>
+            {Array.from({ length: 11 }, (_, i) => (
+              <Text key={i}>{i}</Text>
+            ))}
+          </View>
+          <Text style={styles.sliderValue}>{sliderValue.toString()}</Text>
+        </View>
+      );
+    } else if (currentPainQuestion === 1) {
+      return (
+        <View>
+          <DropDownPicker
+            items={painChoices}
+            defaultValue={selectedPain}
+            containerStyle={{ height: 40 }}
+            placeholder="Which Pain are you experiencing?"
+            onChangeItem={(item) => {
+              setSelectedPain(item.value);
+              setIsOthersSelected(item.value === 'Others');
+            }}
+          />
+          {isOthersSelected && (
+            <TextInput
+              placeholder="Please specify"
+              onChangeText={(text) => {
+                // Handle the user's input here
+              }}
+            />
+          )}
+        </View>
+      );
+    } else {
+      return null;
+    }
+  };
 
-const painChoices = [
-  'Sharp: Sudden, intense, and localized pain sensation.',
-  'Dull: Aching or throbbing pain that is not sharp or intense.',
-  'Stabbing: Brief, piercing pain.',
-  'Shooting: Pain that radiates or shoots along the wrist or into the hand.',
-  'Burning: Sensation of heat or burning in the wrist.',
-  'Tingling: Prickling or "pins and needles" sensation.',
-  'Stiff: Pain accompanied by a sensation of stiffness or reduced range of motion.',
-  'Numb: Partial or complete loss of sensation in the wrist area.',
-  'Cramping: Pain with a constricting or tightening sensation in the wrist.',
-  'Sore: Tenderness or discomfort in the wrist, often associated with overuse or strain.',
-  'Others',
-];
+  return (
+    <View>
+      <Text style={styles.question}>
+        {painAssessmentQuestions[currentPainQuestion]}
+      </Text>
+      {renderPainAssessmentQuestion()}
+    </View>
+  );
+}
+
+function PhysicalInspectionQuestions({
+  currentQuestion,
+  questions,
+  handleImageLibraryPicker,
+  handleCameraPicker,
+  isPainAssessment,
+}) {
+  return (
+    <View>
+      <Text style={styles.question}>
+        {questions[currentQuestion]}
+      </Text>
+      {currentQuestion === questions.length - 1 && (
+        !isPainAssessment && (
+          <View>
+            <TouchableOpacity
+              onPress={handleImageLibraryPicker}
+              style={styles.imagePicker}
+            >
+              <Icon name="image-outline" size={30} color="white" />
+              <Text style={styles.imagePickerText}>Select from Gallery</Text>
+            </TouchableOpacity>
+
+            <TouchableOpacity
+              onPress={handleCameraPicker}
+              style={styles.cameraPicker}
+            >
+              <Icon name="camera-outline" size={30} color="white" />
+              <Text style={styles.cameraPickerText}>Take a Picture</Text>
+            </TouchableOpacity>
+          </View>
+        )
+      )}
+    </View>
+  );
+}
+
 const styles = StyleSheet.create({
 container: {
     padding: 40,
