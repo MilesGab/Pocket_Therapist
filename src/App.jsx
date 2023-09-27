5bd79b81c88ba043774a088b9b55ed2cfa291ab2
MicoRxz <139572357+MicoRxz@users.noreply.github.com>
2023-09-27 12:19:37 +0800

    Upload Function implemented
    Upload media function logic completed but UI not complete yet. Additionally, removed unused js files

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { createStackNavigator } from '@react-navigation/stack';
 import HomeScreen from './screens/Homescreen/Homescreen.js';
 import Schedule from './screens/Schedule.js';
-import Contact from './screens/ChatScreen.js';
 import Notifications from './screens/Notifications.js';
 import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -13,6 +12,7 @@ import Register from './screens/Register.js';
 import auth from '@react-native-firebase/auth';
 import Routes from './navigation/Routes.js';
 import { UserProvider } from '../contexts/UserContext.js';
+
 
 
 export default function App() {
