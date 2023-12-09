9029926315eebb25ca13fdbfd2cffca095ee66a0
MilesGab <79153995+MilesGab@users.noreply.github.com>
2023-12-09 20:51:12 +0800

    -a

--- src/screens/ChatFunction/assessments/Exercises.js
+++ src/screens/ChatFunction/assessments/Exercises.js
@@ -12,25 +12,41 @@ const Exercises = ({ route }) => {
   const [isLoading, setLoading] = React.useState(true);
   const [paused, setPaused] = useState(true);
   const [selectedVideos, setSelectedVideos] = useState({});
+  const [editMode, setEditMode] = useState(false);
+  const [otherVideos, setOtherVideos] = useState([]);
+
+  const fetchVideoDetails = async () => {
+    try {
+      const snapshot = await firestore().collection('exerciseVids').get();
+      const videoData = snapshot.docs.map(doc => doc.data());
+  
+      if (videoData) {
+        // Filter videos based on patient_access including patient id
+        const filteredVideos = videoData.filter(video => video.patient_access && video.patient_access.includes(patientData));
+        setVideos(filteredVideos);
+
+        // Filter videos that do not include patientData
+        const otherVideos = videoData.filter(video => !video.patient_access || !video.patient_access.includes(patientData));
+        setOtherVideos(otherVideos);
+
+      const initialSelectedVideos = filteredVideos.reduce((acc, video) => {
+        acc[video.video_id] = true;
+        return acc;
+      }, {});
+
+      setSelectedVideos(initialSelectedVideos);
+      }
+    } catch (error) {
+      console.error('Error fetching video details from the database:', error);
+    } finally {
+      console.log('Videos accessed: ', videos);
+      console.log('Videos not accessed: ', otherVideos);
+      setLoading(false);
+    }
+  };
 
   useEffect(() => {
-    const fetchVideoDetails = async () => {
-      try {
-        const snapshot = await firestore().collection('exerciseVids').get();
-        const videoData = snapshot.docs.map(doc => doc.data());
-
-        if (videoData) {
-          setVideos(videoData);
-        }
-      } catch (error) {
-        console.error('Error fetching video details from the database:', error);
-      } finally {
-        setLoading(false);
-      }
-    };
-
     fetchVideoDetails();
-    console.log(patientData);
   }, []);
 
   const toggleCheckbox = (videoId) => {
@@ -40,13 +56,20 @@ const Exercises = ({ route }) => {
     }));
   };
 
+  useEffect(()=>{
+    console.log(selectedVideos);
+  },[selectedVideos]);
+
   const renderVideo = ({ item }) => {
     return (
-      <View>
-      <BouncyCheckbox
-          value={!!selectedVideos[item.id]}
-          onPress={() => toggleCheckbox(item.id)}
-      />
+      <View style={{display:'flex', flexDirection:'row'}}>
+        {editMode ? (
+          <BouncyCheckbox
+            isChecked={selectedVideos[item.video_id]}
+            onPress={() => toggleCheckbox(item.video_id)}
+          />
+        ) : (null)
+        }
         <TouchableOpacity style={styles.videoContainer}>
           <Text style={styles.videoTitle}>{item.video_name}</Text>
           <Text>{item.video_description}</Text>
@@ -62,26 +85,79 @@ const Exercises = ({ route }) => {
     );
   };
 
-  const handlePress = () => {
-    console.log('Button clicked!');
+  const handlePress = async () => {
+    try {
+      const updatedVideos = Object.entries(selectedVideos)
+      .filter(([key, value]) => value === true)
+      .map(([key, value]) => key);
+      
+      await Promise.all(
+        updatedVideos.map(async video => {
+          const videoRef = firestore().collection('exerciseVids').doc(video);
+          const patientAccess = video.patient_access || [];
+          
+          if (!patientAccess.includes(patientData)) {
+            patientAccess.push(patientData);
+            await videoRef.update({ patient_access: patientAccess });
+          }
+        })
+      );
+
+      const deletedAccess = Object.entries(selectedVideos)
+      .filter(([key, value]) => value === false)
+      .map(([key, value]) => key);
+
+      await Promise.all(
+        deletedAccess.map(async video => {
+          const videoRef = firestore().collection('exerciseVids').doc(video);
+          const patientAccess = video.patient_access || [];
+          
+          // Filter out the patientData from the patientAccess array
+          const updatedPatientAccess = patientAccess.filter(patient => patient !== patientData);
+
+          // Update the video document with the updated patientAccess array
+          await videoRef.update({ patient_access: updatedPatientAccess });
+        })
+      );
+
+      console.log('Successfully updated access to the following videos: ', updatedVideos);
+    } catch (error) {
+      console.error('Error updating videos in the database:', error);
+    }
   };
+
+  const toggleEdit = () => {
+    setEditMode(!editMode);
+    fetchVideoDetails();
+  }
 
   return (
     <View style={styles.container}>
       <View style={styles.heading}>
-        <Text style={styles.headingTxt}>Exercises</Text>
+        <Text style={[styles.headingTxt, {flex:1}]}>Exercises</Text>
+        <TouchableOpacity onPress={toggleEdit}>
+        {editMode ? (
+          <Text style={[styles.headingTxt, {fontSize: 12, alignContent:'center', fontWeight:'normal', color:'orange'}]}>Cancel</Text>
+        ) : (
+          <Text style={[styles.headingTxt, {fontSize: 12, alignContent:'center', fontWeight:'normal', color:'orange'}]}>Edit</Text>
+        )}
+        </TouchableOpacity>
       </View>
       {isLoading ? (
         <ActivityIndicator size="large" color='#CEDDF7' style={styles.loading} />
       ) : (
         <FlatList
           style={styles.vidlist}
-          data={videos}
+          data={editMode ? [...otherVideos, ...videos] : videos}
           keyExtractor={(item) => item.video_id.toString()}
           renderItem={renderVideo}
         />
       )}
-      <Button title="Click me" onPress={handlePress} />
+      {editMode ? (
+      <Button title="Confirm Changes" onPress={handlePress} />
+      ) : (
+        null
+      )}
     </View>
   );
 };
@@ -97,7 +173,9 @@ const styles = StyleSheet.create({
 
   heading: {
     paddingTop: 30,
-    paddingLeft: 20
+    paddingHorizontal: 20,
+    display:'flex',
+    flexDirection:'row'
   },
 
   headingTxt: {
